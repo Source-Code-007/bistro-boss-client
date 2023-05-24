f8987da4365403538bbf0729c15425391814e7f6
Source-Code-007 <utsho926@gmail.com>
2023-05-24 16:56:13 +0600

    pagination added via array in popular menu section

--- src/Pages/Homepage/PopularMenu/PopularMenu.jsx
+++ src/Pages/Homepage/PopularMenu/PopularMenu.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import UseMenuContext from "../../../Context/UseMenuContext";
 import CommonItemsLayout from "../../../HelpingCompo/CommonItemsLayout";
 import UseSectionTitle from "../../../HelpingCompo/UseSectionTitle";
 
 const PopularMenu = () => {
     const [menu] = UseMenuContext()
+
+    const [itemsPerPage, setItemsPerPage] = useState(6)
+    const [currentPage, setCurrentPage] = useState(0)
+    const totalPage = [...Array(Math.ceil(menu.length / itemsPerPage)).keys()]
+
+    // Calculate starting and ending indices for the current page
+    const startIndex = (currentPage - 0) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
     return (
         <div className="py-16 my-container">
             <UseSectionTitle
@@ -13,9 +23,14 @@ const PopularMenu = () => {
 
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 py-10">
                 {
-                    menu.map(item => <CommonItemsLayout key={item._id} item={item}></CommonItemsLayout>)
+                    menu.slice(startIndex, endIndex).map(item => <CommonItemsLayout key={item._id} item={item}></CommonItemsLayout>)
                 }
             </div>
+            {/* pagination button */}
+            <div className="text-center space-x-2">
+            {totalPage.map((page, ind) => <button key={ind} onClick={() => setCurrentPage(page)} className={`btn btn-success px-5 ${page!==currentPage? 'btn-outline' : '' }`}>{page}</button>)}
+            </div>
+            
         </div>
     );
 };
