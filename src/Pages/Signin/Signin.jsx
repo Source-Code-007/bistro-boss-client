257d1b988a18bef62a04eef688be9d3a06871851
Source-Code-007 <utsho926@gmail.com>
2023-05-25 22:57:23 +0600

    signin page pollish

--- src/Pages/Signin/Signin.jsx
+++ src/Pages/Signin/Signin.jsx
@@ -1,13 +1,16 @@
+import signinLottie from '../../../public/38435-register.json'
+import Lottie from "lottie-react";
+
 const Signin = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left w-3/6">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <div className="text-center lg:text-left w-3/6 flex justify-center items-center">
+                    <Lottie animationData={signinLottie} loop={true} />
                 </div>
-                <div className="card w-3/6 max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-1 shadow-2xl bg-base-100">
                     <div className="card-body">
+                        <h2 className='font-bold text-3xl'>Please Login</h2>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
