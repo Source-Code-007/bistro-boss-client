4787507cd5312d0cb9b6069f7bf31f294572562f
Source-Code-007 <utsho926@gmail.com>
2023-05-27 10:07:48 +0600

    react hook form added in signin

--- src/Pages/Signin/Signin.jsx
+++ src/Pages/Signin/Signin.jsx
@@ -3,42 +3,42 @@ import signinLottie from '../../../public/38435-register.json'
 import Lottie from "lottie-react";
 import { useContext } from 'react';
 import { authContextData } from '../../Context/AuthContext';
+import { useForm } from 'react-hook-form';
+
 
 const Signin = () => {
-    const {signinFunc} = useContext(authContextData)
+    const { signinFunc } = useContext(authContextData)
 
-    const handleSubmitFunc = (e)=>{
-        e.preventDefault()
-        const form = e.target
-        const email = form.email.value
-        const password = form.password.value
-        console.log({email, password});
-        signinFunc(email, password)
-        .then(res=> res.json())
-        .then(data=> console.log(data))
-        .catch(e=> console.log(e.message))
-    }
-    
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const handleSubmitFunc = data => {
+        console.log(data);
+        // signinFunc(email, password)
+        // .then(res => res.json())
+        // .then(data => console.log(data))
+        // .catch(e => console.log(e.message))
+    };
+
     return (
         <div className="hero min-h-screen w-full bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left w-3/6 flex justify-center items-center">
                     <Lottie animationData={signinLottie} loop={true} />
                 </div>
-                <form onSubmit={handleSubmitFunc} className="card flex-1 shadow-2xl bg-base-100">
+                <form onSubmit={handleSubmit(handleSubmitFunc)} className="card flex-1 shadow-2xl bg-base-100">
                     <div className="card-body">
                         <h2 className='font-bold text-3xl'>Please Login</h2>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                            <input type="email" {...register("email", {required: true})} placeholder="email" className="input input-bordered" />
+                            {errors.email && <span>This field is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                            <input type="password" {...register("password", {required: true})} placeholder="password" className="input input-bordered" />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
