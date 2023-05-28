9225195eb4d207c322ec84d33fcb0a127e01d9e5
Source-Code-007 <utsho926@gmail.com>
2023-05-28 13:00:19 +0600

    menu item loaded by category via react tab in yummy shop

--- src/Pages/YummyShop/YummyShop.jsx
+++ src/Pages/YummyShop/YummyShop.jsx
@@ -3,6 +3,7 @@ import CommonParallex from "../../HelpingCompo/CommonParallex";
 import yummyShopBG from '../../assets/shop/banner21.jpg'
 import UseMenu from "../../CustomHook/UseMenu";
 import { useState } from "react";
+import { FaCartPlus } from "react-icons/fa";
 
 const YummyShop = () => {
     const [menu] = UseMenu()
@@ -20,14 +21,33 @@ const YummyShop = () => {
                 <Tabs>
                     <TabList className='flex justify-center gap-2'>
                         {menuItems.map(menuItem => {
-                            return <Tab onClick={()=> setSelectedTab(menuItem)} key={menuItem} className={`${menuItem === selectedTab? 'bg-red-500  text-white' : 'border border-red-500 text-black'} font-semibold p-5 cursor-pointer`}>{menuItem}</Tab>
+                            return <Tab onClick={()=> setSelectedTab(menuItem)} key={menuItem+3} className={`${menuItem === selectedTab? 'bg-red-500  text-white' : 'border border-red-500 text-black'} font-semibold p-5 cursor-pointer`}>{menuItem}</Tab>
                         })}
                     </TabList>
 
                     {
                         menuItems.map(menuItem => {
                             return <TabPanel key={menuItem} >
-                                <h2>{menuItem}</h2>
+                                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+                                    {
+                                         menu.filter(item=> item.category === menuItem).map(yummyItem=>{
+                                            const {name, recipe, category, image} = yummyItem
+                                            // const {}
+                                            return <div key={name+69} className="card bg-base-100 shadow-xl">
+                                            <figure className="px-10 pt-10">
+                                              <img src={image} alt="Shoes" className="rounded-xl" />
+                                            </figure>
+                                            <div className="card-body items-center text-center">
+                                              <h2 className="card-title">{name}</h2>
+                                              <p>{recipe}</p>
+                                              <div className="card-actions">
+                                                <button className="btn btn-error btn-outline gap-2">Add to cart <FaCartPlus></FaCartPlus> </button>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        })
+                                    }
+                                </div>
                             </TabPanel>
                         })
                     }
