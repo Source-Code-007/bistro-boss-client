a5cab36f7345159225962ab118cbfe6790e51b1b
Source-Code-007 <utsho926@gmail.com>
2023-05-29 13:17:40 +0600

    add to cart functionality implement

--- src/Pages/YummyShop/YummyShop.jsx
+++ src/Pages/YummyShop/YummyShop.jsx
@@ -5,28 +5,44 @@ import UseMenu from "../../CustomHook/UseMenu";
 import { useContext, useState } from "react";
 import { FaCartPlus } from "react-icons/fa";
 import { authContextData } from "../../Context/AuthContext";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const YummyShop = () => {
-    const {user} = useContext(authContextData)
+    const { user } = useContext(authContextData)
     const [menu] = UseMenu()
     const [selectedTab, setSelectedTab] = useState('salad')
     const menuItems = ['salad', 'pizza', 'soup', 'dessert', 'drinks']
 
     // handle add to cart func
-    const handleAddToCartFunc = (item)=>{
+    const handleAddToCartFunc = (item) => {
+        const itemId = item._id
         delete item._id
-        const newItem = {...item, email: user.email, id: item._id}
+        const newItem = { ...item, email: user.email, itemId }
         const option = {
             method: 'POST',
-            headers:{
+            headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(newItem) 
+            body: JSON.stringify(newItem)
         }
         fetch('http://localhost:2500/cart-item', option)
-        .then(res=> res.json())
-        .then(data=> console.log(data))
-        .catch(e=> console.log(e.message))
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    toast.success('Item added in cart!', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        });
+                }
+            })
+            .catch(e => console.log(e.message))
     }
 
     return (
@@ -41,29 +57,29 @@ const YummyShop = () => {
                 <Tabs>
                     <TabList className='flex justify-center gap-2'>
                         {menuItems.map(menuItem => {
-                            return <Tab onClick={()=> setSelectedTab(menuItem)} key={menuItem+3} className={`${menuItem === selectedTab? 'bg-red-500  text-white' : 'border border-red-500 text-black'} font-semibold p-5 cursor-pointer`}>{menuItem}</Tab>
+                            return <Tab onClick={() => setSelectedTab(menuItem)} key={menuItem + 3} className={`${menuItem === selectedTab ? 'bg-red-500  text-white' : 'border border-red-500 text-black'} font-semibold p-5 cursor-pointer`}>{menuItem}</Tab>
                         })}
                     </TabList>
 
                     {
-                        menuItems.map(menuItem => {
-                            return <TabPanel key={menuItem} >
+                        menuItems.map((menuItem, ind) => {
+                            return <TabPanel key={menuItem+ind} >
                                 <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 py-12">
                                     {
-                                         menu.filter((item)=> item.category === menuItem).map((yummyItem)=>{
-                                            const {_id, name, recipe, category, image} = yummyItem
+                                        menu.filter((item) => item.category === menuItem).map((yummyItem) => {
+                                            const { _id, name, recipe, image } = yummyItem
                                             return <div key={_id} className="card bg-base-100 shadow-xl">
-                                            <figure>
-                                              <img src={image} alt="Shoes" className="rounded-t-xl w-full h-80" />
-                                            </figure>
-                                            <div className="card-body items-center text-center">
-                                              <h2 className="card-title">{name}</h2>
-                                              <p>{recipe}</p>
-                                              <div className="card-actions">
-                                                <button onClick={()=> handleAddToCartFunc(yummyItem)} className="btn btn-error btn-outline gap-2">Add to cart <FaCartPlus></FaCartPlus> </button>
-                                              </div>
+                                                <figure>
+                                                    <img src={image} alt="Shoes" className="rounded-t-xl w-full h-80" />
+                                                </figure>
+                                                <div className="card-body items-center text-center">
+                                                    <h2 className="card-title">{name}</h2>
+                                                    <p>{recipe}</p>
+                                                    <div className="card-actions">
+                                                        <button onClick={() => handleAddToCartFunc(yummyItem)} className="btn btn-error btn-outline gap-2">Add to cart <FaCartPlus></FaCartPlus> </button>
+                                                    </div>
+                                                </div>
                                             </div>
-                                          </div>
                                         })
                                     }
                                 </div>
@@ -73,6 +89,7 @@ const YummyShop = () => {
 
                 </Tabs>
             </div>
+            <ToastContainer /><ToastContainer />
         </div>
     );
 };
