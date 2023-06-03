cddc2c25100a6fdae8ce233b0fbbf7dc16507b35
Source-Code-007 <utsho926@gmail.com>
2023-06-04 00:04:42 +0600

    client secret successfully received

--- src/Dashboard/UserDashboard/Payment/CheckoutForm.jsx
+++ src/Dashboard/UserDashboard/Payment/CheckoutForm.jsx
@@ -1,14 +1,30 @@
-import { CardElement, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import UseAxiosSecure from "../../../CustomHook/UseAxiosSecure";
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ price }) => {
     const stripe = useStripe();
     const elements = useElements();
+    const [clientSecretP, setClientSecretP] = useState('')
+    const [axiosSecure] = UseAxiosSecure()
     const [error, setError] = useState('')
+
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("http://localhost:2500/create-payment-intent", {
+            method: "POST",
+            headers: { "Content-Type": "application/json", Authorization: localStorage.getItem('jwt-token') },
+            body: JSON.stringify({ price }),
+        })
+            .then((res) => res.json())
+            .then((data) => {console.log(data); setClientSecretP(data.clientSecret)});
+    }, [price]);
 
     // handle payment submit func
     const handlePaymentSubmit = async (e) => {
         e.preventDefault()
+
         if (!elements) {
             return
         }
@@ -16,10 +32,25 @@ const CheckoutForm = () => {
         // Trigger from validation and wallet collection
         const { error: submitError } = await elements.submit()
         if (submitError) {
+            console.log(submitError.message);
             setError(submitError.message)
             return
         }
 
+
+
+        //   const {client_secret: clientSecret} = await res.json();
+
+        // const res = await axiosSecure.post('/create-payment-intent',{price}).then(res=> console.log(res.data))
+        // const  {clientSecret} = await res.json();
+
+        // console.log(clientSecret);
+
+        // const {error} = stripe.confirmPayment({
+
+        // })
+
+        setError('')
 
 
     }
--- src/Dashboard/UserDashboard/Payment/UserDashboardPayment.jsx
+++ src/Dashboard/UserDashboard/Payment/UserDashboardPayment.jsx
@@ -2,31 +2,52 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
 import { loadStripe } from "@stripe/stripe-js";
 import UseCartItem from "../../../CustomHook/UseCartItem";
+import { Oval } from "react-loader-spinner";
 
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK)
-const options = {
-    mode: 'payment',
-    amount: 1099,
-    currency: 'usd',
-    // Fully customizable with appearance API.
-    appearance: {
-      /*...*/
-    },
-  };
 
 const UserDashboardPayment = () => {
     const { cartItems, isLoading, refetch } = UseCartItem()
-    const totalPrice = cartItems?.reduce((acc, item)=> acc+item.price*item.quantity, 0)
-    console.log(totalPrice);
+
+    if (isLoading) {
+        return <div className='min-h-screen flex justify-center items-center'><Oval
+            height={80}
+            width={80}
+            color="#4fa94d"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel='oval-loading'
+            secondaryColor="#4fa94d"
+            strokeWidth={2}
+            strokeWidthSecondary={2}
+
+        /></div>
+    }
+
+
+    const totalPrice = cartItems?.reduce((acc, item) => acc + item.price * item.quantity, 0)
+    const price = parseFloat(totalPrice.toFixed(2))
+
+
+    const options = {
+        mode: 'payment',
+        amount: price * 100,
+        currency: 'usd',
+        // Fully customizable with appearance API.
+        appearance: {
+            theme: 'stripe',
+        },
+    };
 
     return (
         <div className="min-h-screen bg-slate-100 flex justify-center items-center">
             <div className="bg-slate-50 shadow p-20">
                 <h2 className="py-8 font-bold text-3xl text-center">Payment</h2>
-            <Elements stripe={stripePromise} options={options}>
-                <CheckoutForm></CheckoutForm>
-            </Elements>
+                <Elements stripe={stripePromise} options={options}>
+                    <CheckoutForm price={price}></CheckoutForm>
+                </Elements>
             </div>
         </div>
     );
