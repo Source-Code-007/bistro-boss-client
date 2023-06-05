e6a0f4420f31bcfd724b62728c5e1f6bd0c188cf
Source-Code-007 <utsho926@gmail.com>
2023-06-05 11:29:59 +0600

    payment history done

--- src/Dashboard/UserDashboard/UserDashboardPaymentHistory.jsx
+++ src/Dashboard/UserDashboard/UserDashboardPaymentHistory.jsx
@@ -20,6 +20,8 @@ const UserDashboardPaymentHistory = () => {
         },
         enabled: !!user
     })
+
+
     return (
         <div className="min-h-screen flex items-center bg-slate-200">
             <div className="overflow-x-auto w-5/6 mx-auto py-20">
@@ -37,31 +39,33 @@ const UserDashboardPaymentHistory = () => {
                         />
                     </div> :
                         <div className="p-10 bg-slate-50">
-                            <h2>Total Payment: </h2>
+                            <h2 className="font-bold text-3xl my-3">Total Payment: ${paymentInfo.reduce((sum, pInfo)=> sum+pInfo.amount, 0)/100} </h2>
                             <table className="table w-full">
                                 {/* head */}
                                 <thead>
                                     <tr>
                                         <th>#</th>
                                         <th>Email</th>
-                                        <th>Category</th>
-                                        <th className="text-center">Total Price</th>
+                                        <th>trxId</th>
+                                        <th>Total Price</th>
                                         <th>Payment Date</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {/* row 1 */}
                                     {paymentInfo?.map((pInfo, ind) => {
-                                        const { trxId, email, , quantity, price } = pInfo
-                                        return <tr key={name + ind + price}>
+                                        const {_id, trxId, email, user, amount, date } = pInfo
+                                        const dateP = new Date(date).toLocaleDateString('en-US', {
+                                            day: 'numeric',
+                                            month: 'short',
+                                            year: 'numeric',
+                                          })
+                                        return <tr key={_id}>
                                             <td>{ind + 1}</td>
-                                            <td>
-                                                <img className="rounded-full h-10 w-10" src={image} />
-                                            </td>
-                                            <td>{name}</td>
-                                            <td className="text-center">{quantity}</td>
-                                            <td>{price}</td>
-                                            <td><span onClick={() => handleDeleteCartItemFunc(_id)} className="text-red-500 text-xl cursor-pointer"><FaTrash></FaTrash></span></td>
+                                            <td>{email}</td>
+                                            <td>{trxId}</td>
+                                            <td>{amount/100}</td>
+                                            <td>{dateP}</td>
                                         </tr>
                                     })}
                                 </tbody>
