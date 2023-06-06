c33272bb1e3834f6ba9b88b353efa6e4b96c2437
Source-Code-007 <utsho926@gmail.com>
2023-06-06 13:18:34 +0600

    make role functionality added in users management pages

--- src/Dashboard/AdminDashboard/AdminDashboardAllUsers.jsx
+++ src/Dashboard/AdminDashboard/AdminDashboardAllUsers.jsx
@@ -2,7 +2,7 @@
 import UseSectionTitle from "../../HelpingCompo/UseSectionTitle";
 import { Puff } from "react-loader-spinner";
 import { ToastContainer, toast } from "react-toastify";
-import { FaTrash } from "react-icons/fa";
+import { FaTrash, FaUser, FaUserAstronaut } from "react-icons/fa";
 import Swal from "sweetalert2";
 import { useQuery } from "@tanstack/react-query";
 import UseAxiosSecure from "../../CustomHook/UseAxiosSecure";
@@ -13,7 +13,7 @@ const AdminDashboardAllUsers = () => {
     const { isLoading, error, data: users, refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () =>
-          await axiosSecure.get('/users').then(res=> res.data).catch(e=> console.log(e.message))
+            await axiosSecure.get('/users').then(res => res.data).catch(e => console.log(e.message))
     })
 
     // handle delete user func
@@ -29,10 +29,9 @@ const AdminDashboardAllUsers = () => {
             confirmButtonText: 'Yes, delete it!'
         }).then((result) => {
             if (result.isConfirmed) {
-                fetch(`http://localhost:2500/users/${id}`, { method: 'DELETE', headers: { Authorization: localStorage.getItem('jwt-token') } })
-                    .then(res => res.json())
+                axiosSecure.delete(`users/${id}`)
                     .then(data => {
-                        if (data.deletedCount > 0) {
+                        if (data.data.deletedCount > 0) {
                             refetch() // refetch users after delete a user
                             toast.success('Item deleted successfully!', {
                                 position: "top-right",
@@ -50,6 +49,16 @@ const AdminDashboardAllUsers = () => {
             }
         })
 
+    }
+
+    // make admin func
+    const makeRoleFunc = (id, role) => {
+        axiosSecure.patch(`/make-role/${id}`, {role: role})
+            .then(res => {
+                refetch()
+                console.log(res)
+            })
+            .catch(e => console.log(e.message))
     }
 
     return (
@@ -92,7 +101,7 @@ const AdminDashboardAllUsers = () => {
                                 </thead>
                                 <tbody>
                                     {/* row 1 */}
-                                    {users.length&& users.map((user, ind) => {
+                                    {users.length && users.map((user, ind) => {
                                         const { _id, name, email, photo, role } = user
                                         return <tr key={_id}>
                                             <td>{ind + 1}</td>
@@ -101,7 +110,7 @@ const AdminDashboardAllUsers = () => {
                                             </td>
                                             <td>{name}</td>
                                             <td className="text-center">{email}</td>
-                                            <td>{role}</td>
+                                            <td> <p>{role}</p> <span onClick={() => makeRoleFunc(_id, role)} className="btn btn-sm btn-error"> {role === 'user' ? 'make admin' : 'make user'}</span> </td>
                                             <td><span onClick={() => handleDeleteUserFunc(_id)} className="text-red-500 text-xl cursor-pointer"><FaTrash></FaTrash></span></td>
                                         </tr>
                                     })}
